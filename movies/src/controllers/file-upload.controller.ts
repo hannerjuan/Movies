a62c831b4cacaefeef6b7c3745313582224db4e7
Hanner Obando <hannerobando@outlook.com>
2020-09-27 01:07:30 -0500

    File upload controller is added

---
+++ movies/src/controllers/file-upload.controller.ts
@@ -0,0 +1,135 @@
+import {inject} from '@loopback/core';
+import {repository} from '@loopback/repository';
+import {
+  HttpErrors,
+  post,
+  Request,
+  requestBody,
+  Response,
+  RestBindings,
+} from '@loopback/rest';
+import multer from 'multer';
+import path from 'path';
+import {UploadFilesKeys} from '../keys/upload-files-keys';
+import {Movie} from '../models';
+import {
+  MovieRepository,
+} from '../repositories';
+
+export class FileUploadController {
+  /**
+   *
+   * @param MovieRepostory
+   */
+  constructor(
+    @repository(MovieRepository)
+    private movieRepository: MovieRepository
+  ) {}
+
+  //POST PARA MOVIE IMAGE
+
+  /**
+   *
+   * @param response
+   * @param movieId
+   * @param request
+   */
+  @post('/movieImage', {
+    responses: {
+      200: {
+        content: {
+          'application/json': {
+            schema: {
+              type: 'object',
+            },
+          },
+        },
+        description: 'Movie Image',
+      },
+    },
+  })
+  async movieImageUpload(
+    @inject(RestBindings.Http.RESPONSE) response: Response,
+    @requestBody.file() request: Request,
+  ): Promise<object | false> {
+    const movieImagePath = path.join(
+      __dirname,
+      UploadFilesKeys.MOVIE_IMAGE_PATH,
+    );
+    let res = await this.StoreFileToPath(
+      movieImagePath,
+      UploadFilesKeys.MOVIE_IMAGE_FIELDNAME,
+      request,
+      response,
+      UploadFilesKeys.IMAGE_ACCEPTED_EXT,
+    );
+    if (res) {
+      const filename = response.req?.file.filename;
+      if (filename) {
+        return {filename: filename};
+      }
+    }
+    return res;
+  }
+
+  /**
+   * store the file in a specific path
+   * @param storePath
+   * @param request
+   * @param response
+   */
+  private StoreFileToPath(
+    storePath: string,
+    fieldname: string,
+    request: Request,
+    response: Response,
+    acceptedExt: string[],
+  ) {
+    return new Promise<object>((resolve, reject) => {
+      const storage = this.GetMulterStorageConfig(storePath);
+      const upload = multer({
+        storage: storage,
+        fileFilter: function (req, file, callback) {
+          var ext = path.extname(file.originalname).toUpperCase();
+          console.log('Extensión del archivo: ' + ext);
+          console.log(acceptedExt);
+          console.log(acceptedExt.includes(ext));
+          if (acceptedExt.includes(ext)) {
+            return callback(null, true);
+          }
+          return callback(
+            new HttpErrors[400]('This format file is not supported.'),
+          );
+        },
+        limits: {
+          fileSize: UploadFilesKeys.MAX_FILE_SIZE,
+        },
+      }).single(fieldname);
+      upload(request, response, (err: any) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(response);
+      });
+    });
+  }
+
+  /**
+   * Return a config for multer storage
+   * @param path
+   */
+  // Donde lo voy a guardar y como lo voy a guardar y con que nombre y en que ruta
+  private GetMulterStorageConfig(path: string) {
+    var filename: string = '';
+    const storage = multer.diskStorage({
+      destination: function (req, file, cb) {
+        cb(null, path);
+      },
+      filename: function (req, file, cb) {
+        filename = `${Date.now()}-${file.originalname}`;
+        cb(null, filename);
+      },
+    });
+    return storage;
+  }
+}
---
+++ movies/src/keys/upload-files-keys.ts
@@ -0,0 +1,6 @@
+export namespace UploadFilesKeys {
+    export const MOVIE_IMAGE_PATH = '../../uploads/movie/';
+    export const MOVIE_IMAGE_FIELDNAME = 'file';
+    export const MAX_FILE_SIZE = 1024 * 1024;
+    export const IMAGE_ACCEPTED_EXT: string[] = ['.PNG', '.JPG', '.JPEG'];
+}
